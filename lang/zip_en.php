1f632d512587451dd58a736772c18291582c14f0
gizmore <gizmore@wechall.net>
2022-07-24 22:13:31 +0200

    '7.0.1-r1401'

--- lang/zip_en.php
+++ lang/zip_en.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'ft_zip_admin' => 'ZIP Detection',
+    'mt_zip_admin' => 'ZIP Detection',
     'detect_zip' => 'Detect',
     'cfg_gzip_binary' => 'GZIP Path',
     'cfg_zip_binary' => 'ZIP Path',
