f746fca55ba90a76ff6e535ac5604f076b4c1a07
gizmore <gizmore@wechall.net>
2022-10-07 07:02:33 +0200

    '7.0.1-r1649 German Lang Pack - I'

---
+++ lang/zip_de.php
@@ -0,0 +1,4 @@
+<?php
+namespace GDO\ZIP\lang;
+return [
+    'mt_zip_admin' => 'ZIP-Erkennung','detect_zip' => 'Erkennen','cfg_gzip_binary' => 'GZIP-Pfad','cfg_zip_binary' => 'ZIP-Pfad','cfg_gunzip_binary' => 'GUNZIP-Pfad','msg_zip_detected' => 'Die Zip-Binärdatei wurde erkannt','msg_gzip_detected' => 'Die gzip-Binärdatei wurde erkannt',];
